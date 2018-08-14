a71468dfdff28247eeef0b33f850062aac865694
Harry Cao <1070170859@qq.com>
2018-08-14 20:15:00 +0800

    ！！！

---
+++ src/main/java/com/harry/demo/GirlApplication.java
@@ -0,0 +1,13 @@
+package com.harry.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GirlApplication.class, args);
+    }
+}
---
+++ src/main/java/com/harry/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.harry.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+        private GirlProperties girlProperties;
+
+    /*@RequestMapping(value = "/Hello",method = RequestMethod.GET)*///配置url
+    @GetMapping(value = {"/hi/{id}","/Hello"})
+    private String say(@PathVariable("id") Integer id){//通过PathVariable来获取url中的值
+                    /*(@RequestParam(value="id",required=false(不必传),defaultValue="0"(设定默认值，只能是字符串)) Integer id)//通过RequestParam来获取？id=189 这种格式的url值*/
+
+        /*return girlProperties.getCupSize();*/
+        return "id: " +id;
+    }
+}
